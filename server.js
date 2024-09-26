e48b7dcc261bcf793ce772726337c47f2562c2b8
Embaren2 <77256554+Embaren@users.noreply.github.com>
2024-09-27 00:34:08 +0200

    fixed forced hosting IP

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const {loadJSONSync} = require('./scripts/utils_back.js');
 const {CommandPrompt} = require('./scripts/commands_back.js');
 const {Game} = require('./scripts/game_back.js');
 
-const IP = process.env.IP || 'localhost';
+//const IP = process.env.IP || 'localhost';
 const PORT = process.env.PORT || 3000;
 
 const gameConfig = loadJSONSync("./config.json");
@@ -26,12 +26,12 @@ app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(PORT, IP, ()=>{
-    console.log(`listening on ${IP}:${PORT}`);
+server.listen(PORT, ()=>{
+    console.log(`listening on :${PORT}`);
 });
 server.restart=()=>{
     server.close();
-    server.listen(PORT, IP, ()=>{
-        console.log(`listening on ${IP}:${PORT}`);
+    server.listen(PORT, ()=>{
+        console.log(`listening on :${PORT}`);
     });
 }
